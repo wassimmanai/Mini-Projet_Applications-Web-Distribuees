aca5ff8a2baffe493cdbe352cc7d046d7e1bba88
wassimmanai <wassim.manai@esprit.tn>
2024-11-08 07:54:07 +0100

    Apigateway

---
+++ Apigateway/src/main/java/com/example/apigateway/ApigatewayApplication.java
@@ -0,0 +1,30 @@
+package com.example.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+
+        return builder.routes()
+                .route("Candidat", r->r.path("/candidats/**")
+                        .uri("http://localhost:8080"))
+
+                .route("job-s", r->r.path("/jobs/**")
+                        .uri("http://localhost:8082"))
+                .build();
+    }
+}
