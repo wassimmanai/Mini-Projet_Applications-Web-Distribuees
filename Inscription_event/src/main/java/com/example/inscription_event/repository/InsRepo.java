0c1b7f3e2fe276423619ded8a4ce6a4c092ee8dc
wassimmanai <wassim.manai@esprit.tn>
2024-11-08 20:51:10 +0100

    h2base

--- Inscription_event/src/main/java/com/example/inscription_event/repository/InsRepo.java
+++ Inscription_event/src/main/java/com/example/inscription_event/repository/InsRepo.java
@@ -3,5 +3,5 @@ package com.example.inscription_event.repository;
 import com.example.inscription_event.entity.InsEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InsRepo extends JpaRepository<InsEvent, Integer> {
+public interface InsRepo extends JpaRepository<InsEvent, Long> {
 }
