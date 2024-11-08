f90d3e82e525b48f10c48e4b478dae5f56131844
wassimmanai <wassim.manai@esprit.tn>
2024-11-08 09:10:27 +0100

    Inscri

---
+++ Inscription_event/src/main/java/com/example/inscription_event/InscriptionEventApplication.java
@@ -0,0 +1,13 @@
+package com.example.inscription_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscriptionEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InscriptionEventApplication.class, args);
+	}
+
+}
