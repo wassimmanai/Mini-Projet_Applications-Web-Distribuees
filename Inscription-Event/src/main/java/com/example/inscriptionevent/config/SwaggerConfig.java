e9f0bc74b77dc4085d627f9d74419c63faaa0429
wassimmanai <wassim.manai@esprit.tn>
2024-11-11 02:09:15 +0100

    xx

--- Inscription-Event/src/main/java/com/example/inscriptionevent/config/SwaggerConfig.java
+++ Inscription-Event/src/main/java/com/example/inscriptionevent/config/SwaggerConfig.java
@@ -1,6 +1,6 @@
 package com.example.inscriptionevent.config;
 
-import io.swagger.v3.oas.models.Components;
+/*import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.*;
@@ -9,12 +9,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static sun.net.www.protocol.http.AuthCacheValue.Type.Server;
+import static sun.net.www.protocol.http.AuthCacheValue.Type.Server;*/
+
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
-    private static final String SCHEME_NAME = "Keycloak";
+    /*private static final String SCHEME_NAME = "Keycloak";
     private static final String SCHEME = "bearer";
 
 
@@ -58,5 +60,5 @@ public class SwaggerConfig {
                         )
                 )
                 .scheme(SCHEME);
-    }
+    }*/
 }
